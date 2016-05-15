1cd22b0f90c1ae138eda65e6498cef7090072491
Carlos Antonio <fulvio.ac@gmail.com>
2016-05-14 22:32:49 -0400

    changed getJSON by ajax

--- js/index.js
+++ js/index.js
@@ -2,7 +2,12 @@ $(document).ready(function() {
   $("#getGeo").click(function() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
-        $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+position.coords.latitude+"&lon="+position.coords.longitude+"&appid=8191185ae7a955c5e64f8b6cffa991df", function(result) {
+        $.ajax({
+          url: "http://api.openweathermap.org/data/2.5/weather?lat="+position.coords.latitude+"&lon="+position.coords.longitude+"&appid=8191185ae7a955c5e64f8b6cffa991df",
+          type: 'GET',
+          datatype: 'json',
+          success: function(data) {
+            data.forEach(function(
           $.each(result, function(i, field) {
             $("#location").append(field + " ");
           });
